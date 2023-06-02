a475ab924ecf61cb6e1065a3695475b7609fb899
kcald <kcald@MACHINE2.myguest.virtualbox.org>
2023-06-02 13:35:36 -0700

    docker perm update

--- unit-test.sh
+++ unit-test.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # Run unit tests for the assignment
-#
+
 # Automate these steps from the readme:
 # Create a build subdirectory, change into it, run
 # cmake .. && make && run the assignment-autotest application
