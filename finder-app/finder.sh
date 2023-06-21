b42ad3508a2d2cd108eecc2f15d975917e68da50
kcald <keca2375@colorado.edu>
2023-06-20 18:13:59 -0700

    finder.sh upd

--- finder-app/finder.sh
+++ finder-app/finder.sh
@@ -1,38 +1,23 @@
-#!/bin/bash
-# a script that searcehs a gievn directory on the fgilesytstem for a given text file
-#task1 returns value 1 error if parametners not specificied
-#task2 returnbs value1 error if given directory doesnt match any on file system
-#task 3prints X files and Y strings of matching 
+#!/bin/sh
+bin=$1
 
-#check if all inputs were given
-if [ "$#" -ne 2 ]; then
-	echo "Parameters not specified"
-	exit 1
-fi
-#capture the inputs
-filesdir=$1
-searchstr=$2
 
-#getting directory read errors due to input
-#filesdir=${filesdir%/}
-#echo "filesdir: $filesdir" #terminal check for directory validity
-
-#Error if given directory is not valid 
-if [ ! -d $filesdir ]; then
-	echo "Error: Not a directory: $filesdir" 
+if [ $# -eq 2 ]
+then
+	if [ -d "$1" ]
+	then
+	   	echo ""
+	else
+		echo "$1 is not valid"
+		exit 1
+	fi
+	cd $1
+	Y="$(grep -r $2 * | wc -l)"
+	X="$(grep -r -l $2 * | wc -l)"
+else
+	echo "Failure::Arguments are less than 2"
 	exit 1
 fi
 
-#give counts of matches for files 
-X=$(find "$filesdir" -type f | wc -l)
- echo "Files in #filesdir is $X "
- 
-# Search for the given string in each file within the directory
-Y=$(grep -r -c "$searchstr" "$filesdir" | awk -F':' '{sum += $2} END {print sum}') #was failing for some reason needed awk 	
-
-echo The number of files are $X and the number of matching lines are $Y
-
-
-
-
-
+echo "The number of files are ${X} and the number of matching lines are ${Y}"
+exit 0
